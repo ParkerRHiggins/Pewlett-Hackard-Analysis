78b749992d436089754f2315d60e90a0e1bdd9e8
Parker Higgins <75704021+ParkerRHiggins@users.noreply.github.com>
2021-02-06 10:02:35 -0600

    Adding files thru Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1
 SELECT e.emp_no, 
         e.first_name, 
         e.last_name,
@@ -11,7 +12,7 @@ ON e.emp_no = t.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY t.emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows
+
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
 last_name,
@@ -20,8 +21,28 @@ INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no ASC, to_date DESC;
 
+
 SELECT COUNT(title), title 
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+
+-- Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+		e.first_name, 
+		e.last_name, 
+		e.birth_date, 
+		de.from_date, 
+		de.to_date, 
+		t.title
+INTO mentorship_elegible 
+FROM employees AS e
+LEFT JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+LEFT JOIN titles AS t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
